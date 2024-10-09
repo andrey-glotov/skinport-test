c2068ba4fc5d3a0a0a786d9e66df0d204174c060
a.glotov <andruxa.comm@yandex.ru>
2024-10-09 21:38:20 +0300

    Добавлена миграция на создание админ пользователя

---
+++ src/migrations/1728487081250-CreateAdminUser.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { User } from '~/modules/item/user.entity';
+import { createHash } from '~/shared/utils/create-hash';
+
+require('dotenv').config();
+
+export class CreateAdminUser1728487081250 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const user = await queryRunner.manager
+      .getRepository(User)
+      .createQueryBuilder('user')
+      .where('user.username = :username', {
+        username: process.env.APP_USERNAME,
+      })
+      .getOne();
+
+    if (!user) {
+      await queryRunner.manager
+        .createQueryBuilder()
+        .insert()
+        .into(User)
+        .values([
+          {
+            username: process.env.APP_USERNAME,
+            password: createHash(
+              process.env.APP_PASSWORD,
+              process.env.APP_SECRET_KEY,
+            ),
+          },
+        ])
+        .execute();
+    }
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const user = await queryRunner.manager
+      .getRepository(User)
+      .createQueryBuilder('user')
+      .where('user.username = :username', {
+        username: process.env.APP_USERNAME,
+      })
+      .getOne();
+
+    if (user) {
+      await queryRunner.manager
+        .createQueryBuilder()
+        .delete()
+        .from(User)
+        .where('id = :id', { id: user.id })
+        .execute();
+    }
+  }
+}
